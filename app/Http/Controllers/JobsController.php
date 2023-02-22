7df17c89964195bd9ebce0fcba21d783fa25fa24
mchedlo14 <golocogo4@gmail.com>
2023-02-22 01:21:14 -0800

    initial commit

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jobs;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Jobs $jobs)
+    {
+        //
+    }
+
+
+    public function edit(Jobs $jobs)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Jobs $jobs)
+    {
+        //
+    }
+
+
+    public function destroy(Jobs $jobs)
+    {
+        //
+    }
+}
