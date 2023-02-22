acff6f4855c97afc2400ccb077b2fdf34989d34e
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-02-22 23:05:13 +0400

    router fix

--- react/src/pages/register/index.jsx
+++ react/src/pages/register/index.jsx
@@ -1,9 +1,15 @@
 function RegisterPage() {
-    return ( 
-        <>
-        RegisterPage
-        </>
-     );
+    return (
+        <section className="register-wrapper">
+            <div className="register-contaner">
+                {/* Registration Information */}
+                <div className="registration-information"></div>
+
+                {/* Registration Form */}
+                <div className="registration-form"></div>
+            </div>
+        </section>
+    );
 }
 
 export default RegisterPage;
