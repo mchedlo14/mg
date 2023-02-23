5985cddc3638cbfed65faa03f6fe2483b0944c48
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-02-23 20:28:25 +0400

    stash fixed

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,27 +1,27 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
-import RootLayout from "./components/layout/Rootlayout"
-import HomePage from "./pages/home"
-import RegisterPage from "./pages/register"
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./components/layout/Rootlayout";
+import HomePage from "./pages/home";
+import RegisterPage from "./pages/register";
 
 // Style
-import './styles/main.scss'
-
+import "./styles/main.scss";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<HomePage />}></Route>
-      <Route path="register" element={<RegisterPage />}></Route>
-
-    </Route>
-  )
-)
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout />}>
+            <Route index element={<HomePage />}></Route>
+            <Route path="register" element={<RegisterPage />}></Route>
+        </Route>
+    )
+);
 
 function App() {
-
-  return (
-    <RouterProvider router={router} />
-  )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
