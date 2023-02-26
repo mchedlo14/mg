46508f407a820846df1cfe7e7b43a5592299f08a
mchedlo14 <golocogo4@gmail.com>
2023-02-25 23:56:46 -0800

    added jobs card and card icons

--- react/src/pages/jobs/Jobs.jsx
+++ react/src/pages/jobs/Jobs.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 import searchIcon from '../../assets/svgs/search.svg'
+import clockIcon from '../../assets/images/clock.png'
+import pinIcon from '../../assets/images/pin.png'
+import walletIcon from '../../assets/images/wallet.png'
 const Jobs = () => {
   return (
     <section className='jobs-wrapper'>
@@ -16,8 +19,82 @@ const Jobs = () => {
         </div>
 
         <div className='jobs-search-wrapper'>
-            <input placeholder='მაგ: პროგრამისტი'/>
+            <input placeholder='მაგ: გაყიდვების მენეჯერი'/>
             <img src={searchIcon} alt='search'/>
+        </div>
+
+
+        <div className='jobs-list-wrapper'>
+          <div className='jobs-filter-wrapper'>
+
+          </div>
+
+          <div className='jobs-list-container'>
+            <div className='job-card'>
+              <div className='card-title'>
+                <img src='https://www.epam.com/etc/designs/epam-core/images/common/logo.png'/>
+                <div>
+                  <h3>ქოლ-ცენტრის ოპერატორი</h3>
+                  <p>ჭრელი აბანო</p>
+                </div>
+              </div>
+
+              <div className='card-detail'>
+                <div>
+                  <img src={clockIcon} alt='clock icon'/>
+                  <h5>სრული განაკვეთი</h5>
+                </div>
+
+                <div>
+                  <img src={pinIcon} alt='pin icon'/>
+                  <h5>წერეთლის გამზირი</h5>
+                </div>
+
+                <div>
+                  <img src={walletIcon} alt='clock icon'/>
+                  <h5>900-1100₾</h5>
+                </div>
+              </div>
+
+
+              <div className='date-wrapper'>
+                <p>24 თებ-26 მარ</p>
+              </div>
+            </div>
+
+                        <div className='job-card'>
+              <div className='card-title'>
+                <img src='https://www.epam.com/etc/designs/epam-core/images/common/logo.png'/>
+                <div>
+                  <h3>ქოლ-ცენტრის ოპერატორი</h3>
+                  <p>ჭრელი აბანო</p>
+                </div>
+              </div>
+
+              <div className='card-detail'>
+                <div>
+                  <img src={clockIcon} alt='clock icon'/>
+                  <h5>სრული განაკვეთი</h5>
+                </div>
+
+                <div>
+                  <img src={pinIcon} alt='pin icon'/>
+                  <h5>წერეთლის გამზირი</h5>
+                </div>
+
+                <div>
+                  <img src={walletIcon} alt='clock icon'/>
+                  <h5>900-1100₾</h5>
+                </div>
+              </div>
+
+
+              <div className='date-wrapper'>
+                <p>24 თებ-26 მარ</p>
+              </div>
+            </div>
+          </div>
+
         </div>
 
     </section>
